e9597f058f5f627f8dd534fe2f856801597f412c
Joshua Martinez <jam@Joshuas-MacBook-Pro.local>
2019-09-06 11:25:45 -0500

    pushing work test 1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
 // import axios here
+const axios = require('axios');
+const config =require('../config')
+
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
-const api_key = 'd771b19ef336ed8381def3a60b574464'
+
 
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  axios.get(url)
+    .then(function(response) {
+      console.log(response)
+     })
+    .catch(function(error){
+      console.log(error)
+    })
+    .finally(function(){
+    });
+  
 }
+discoverMovie()
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
+  axios.get(url, {
+    params: {
+      id: 500
+    }
+  })
+
+.then(function(response) {
+  return true
+  console.log(response.data.original_title)
+   
+ })
+.catch(function(error){
+  console.log(error)
+})
+.finally(function(){
+});
+
 }
 
 const getMovieByIdFailure = () => {
